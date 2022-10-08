baff05bbda84f187bb67c26e5f1208c2c9403700
atharvsharma <atharv228.as@gmail.com>
2022-10-08 14:01:01 +0100

    website update

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-let header = document.querySelector('header');
+// let header = document.querySelector('header');
 
-window.addEventListener('scroll', () => {
-    header.classList.toggle('shadow', window.scrollY>0);
-});
+// window.addEventListener('scroll', () => {
+//     header.classList.toggle('shadow', window.scrollY>0);
+// });
+const hamburger = document.querySelector('.hamburger');
+const hamburger_icon = hamburger.querySelector('span');
+const mobile_menu = document.querySelector('.mobile-menu');
+const mobile_menu_link = mobile_menu.querySelector('a');
+
+hamburger.addEventListener('click', () =>{
+    hamburger_icon.innerText = hamburger_icon.innerText === 'menu' ? 'close' : 'menu';
+
+    mobile_menu.classList.toggle('is-open');
+})
+
+mobile_menu.addEventListener('click', () =>{
+    hamburger_icon.innerText = hamburger_icon.innerText === 'menu' ? 'close' : 'menu';
+
+    mobile_menu.classList.toggle('is-open');
+})
